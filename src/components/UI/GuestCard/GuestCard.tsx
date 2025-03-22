75401d9da5ae948681d359b901c2eeecbb42617e
Sam Hutcher <samhutcher@gmail.com>
2025-03-21 19:31:20 -0700

    Style Changes

--- src/components/UI/GuestCard/GuestCard.tsx
+++ src/components/UI/GuestCard/GuestCard.tsx
@@ -1,4 +1,4 @@
-import { JSX } from "react";
+import { act, JSX } from "react";
 import { Guest, GuestAction } from "../../Core/Guests";
 import "./GuestCard.scss";
 
@@ -20,7 +20,7 @@ function ActionIcon() {
 
 function StarIcon() {
 
-    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width={"1em"} fill="currentColor" ><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" /></svg>
+    return <div className="star-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width={"1em"} fill="currentColor" stroke="black" strokeWidth={30}  ><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" /></svg></div>
 }
 
 function StatNumber({ type, value }: { type: string, value: number }) {
@@ -58,7 +58,7 @@ function StatNumber({ type, value }: { type: string, value: number }) {
 
 
 
-function ActionLine({ action }: { action: GuestAction }) {
+function ActionLine({ action,used }: { action: GuestAction ,used:boolean}) {
 
     let desc = "";
     switch (action) {
@@ -97,8 +97,8 @@ function ActionLine({ action }: { action: GuestAction }) {
             break;
     }
 
-    return <div className="action-line">
-
+    return <div className={"action-line "+(used?"used":"")}>
+      <div>A</div>
         <span>{desc}</span>
     </div>
 
@@ -124,12 +124,13 @@ export function GuestCard({ guest,onClick,addClass }: {addClass:string, guest: G
                         {guest.description}
                     </div>
                 }
+
+                <div className="stats-row">
                 {guest.action != GuestAction.None &&
-                    <div className="action-row" style={{ opacity: guest.hasAction ? 1 : 0.5 }}>
-                        <ActionLine action={guest.action} />
+                    <div className="action-row" >
+                        <ActionLine used={!guest.hasAction} action={guest.action} />
                     </div>
                 }
-                <div className="stats-row">
                     <StatNumber type="pop" value={guest.pop} />
                     <StatNumber type="cash" value={guest.cash} />
                     <StatNumber type="trouble" value={guest.trouble} />
